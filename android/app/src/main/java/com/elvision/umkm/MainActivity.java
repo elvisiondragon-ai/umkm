1b183fd09f181ceff3b0f7928213c61312a07ef4
elvision <elvisiondragon@gmail.com>
2026-03-01 18:07:47 +0700

    V223

---
+++ android/app/src/main/java/com/elvision/umkm/MainActivity.java
@@ -0,0 +1,5 @@
+package com.elvision.umkm;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.elvision.umkm',
+  appName: 'UMKM eL Vision',
+  webDir: 'dist'
+};
+
+export default config;
